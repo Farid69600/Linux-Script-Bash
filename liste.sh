a1c96ce2f2ac8496ca3530c44580d197a478f651
Farid <frerebenoit644@gmail.com>
2021-09-03 14:07:24 +0200

    Cours Linux vm 09/2021

---
+++ liste.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+informations=(dauphin licorne meringue)
+
+for info in ${informations[@]}
+    do
+        echo "$info"
+done
+
+#affiche en liste le contenu de "informations"
+#dauphin
+#licorne
+#meringue
